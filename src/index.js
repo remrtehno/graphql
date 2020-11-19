6fe2f18f444753f0750ef7fa2f5b203462acc41d
Kuryakov <igor@mst.uz>
2020-11-19 12:48:17 +0500

    fix: added appollo client for index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: 'https://graphql-pokemon.now.sh/'
+})
+
+const client = new ApolloClient({
+    cache,
+    link
+})
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
